16a567580c3abbe7b8b1319eeeba895832214ed2
itsnotkamana <kamanarmtl08@gmail.com>
2025-03-24 08:56:20 +0545

    Add files via upload

--- dev3_multiplication.py
+++ dev3_multiplication.py
@@ -1,10 +1,6 @@
 def multiply_numbers(num1,num2):
-    num1=float(input("Enter the first number"))
-    num2=float(input("Enter the second number"))
     result = num1 * num2
-    print(f"The product of {num1} and {num2} is {result}")
-multiply_numbers()
-    
+
 
 
 
